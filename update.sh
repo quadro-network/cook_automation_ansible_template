227512ba6706a5ad0209e4f0c4a123e6c132ecf5
Contributor <93165931+quadro-network@users.noreply.github.com>
2025-08-18 01:58:40 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ docker container rm elixir
 echo "Deleted container..."
 docker pull elixirprotocol/validator:v3 --platform linux/amd64
 
-docker run -d --env-file /root/elixir/validator.env --name elixir --platform linux/amd64 -p 17690:17690 elixirprotocol/validator:v3
+docker run -d --env-file /home/validator/elixir/validator.env --name elixir --platform linux/amd64 -p 17690:17690 elixirprotocol/validator:v3
